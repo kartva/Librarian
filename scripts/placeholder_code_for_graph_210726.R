37366869acbd9c89db588c8e27464c4b593e493e
Kartavya Vashishtha <sendtokartavya@gmail.com>
2021-08-18 00:16:23 +0530

    Make it actually work

--- scripts/placeholder_code_for_graph_210726.R
+++ scripts/placeholder_code_for_graph_210726.R
@@ -1,5 +1,3 @@
-#!/usr/bin/env Rscript
-
 ## This script reads in the base compositions of the 2019 GEO search which is filtered for the most frequent library types and limits the number of examples 
 ## of one library to 400.
 ## Base compositions are fed into UMAP to get a two dimensional representation of the data.
@@ -13,13 +11,18 @@
 library(umap)
 library(tidyverse)
 
-
-## Setting working directory
+## Getting input file path
+args = commandArgs()
 
 ## Loading gathered library compositions
 
 read_tsv("data/output_210709.tsv") -> output
+read_tsv(stdin(), col_names = F) -> test.import
 
+## header for the handling of the test library
+
+header <- as.character(expression(A1, C1, G1, T1, N1, A2, C2, G2, T2, N2, A3, C3, G3, T3, N3, A4, C4, G4, T4, N4, A5, C5, G5, T5, N5, A6, C6, G6, T6, N6, A7, C7, G7, T7, N7, A8, C8, G8, T8, N8, A9, C9, G9, T9, N9, A10, C10, G10, T10, N10, A11, C11, G11, T11, N11, A12, C12, G12, T12, N12, A13, C13, G13, T13, N13, A14, C14, G14, T14, N14, A15, C15, G15, T15, N15, A16, C16, G16, T16, N16, A17, C17, G17, T17, N17, A18, C18, G18, T18, N18, A19, C19, G19, T19, N19, A20, C20, G20, T20, N20, A21, C21, G21, T21, N21, A22, C22, G22, T22, N22, A23, C23, G23, T23, N23, A24, C24, G24, T24, N24, A25, C25, G25, T25, N25, A26, C26, G26, T26, N26, A27, C27, G27, T27, N27, A28, C28, G28, T28, N28, A29, C29, G29, T29, N29, A30, C30, G30, T30, N30, A31, C31, G31, T31, N31, A32, C32, G32, T32, N32, A33, C33, G33, T33, N33, A34, C34, G34, T34, N34, A35, C35, G35, T35, N35, A36, C36, G36, T36, N36, A37, C37, G37, T37, N37, A38, C38, G38, T38, N38, A39, C39, G39, T39, N39, A40, C40, G40, T40, N40, A41, C41, G41, T41, N41, A42, C42, G42, T42, N42, A43, C43, G43, T43, N43, A44, C44, G44, T44, N44, A45, C45, G45, T45, N45, A46, C46, G46, T46, N46, A47, C47, G47, T47, N47, A48, C48, G48, T48, N48, A49, C49, G49, T49, N49, A50, C50, G50, T50, N50
+))
 
 ## Some tidying
 
@@ -33,7 +36,14 @@ output %>%
   distinct(srr_number, .keep_all = T) -> compositions # deduplicating SRR numbers (I checked, some are duplicated, they are carried through from 
                                                       # the original search from entries with very similar titles. They appear to be the same data sets.)
 
+colnames(test.import) <- header
 
+test.import %>% 
+  drop_na() %>%  # get rid of the ones that don't have compositions
+  mutate(Nsum = rowSums(select(.,starts_with("N")))) %>% 
+  filter(Nsum < 301) %>% # should give a warning here instead
+  drop_na() %>% 
+  select(-starts_with("N"), -Nsum) -> test # disregarding Ns
 
 ## Arranging data for UMAP
 
@@ -45,12 +55,10 @@ compositions %>%
  select(serial_num, species, lib_type, srr_number, title) -> umap.annotations
 
 
-## Making up an example to act as test data
+## Arranging test data
 
-compositions %>% 
-  slice(1) %>% 
-  select(-species, -lib_type, -serial_num, -URL, - title) %>% 
-  column_to_rownames("srr_number") -> umap.test
+test %>% 
+  as.data.frame() -> test.data
 
 
 ## setting global seed which should be used by umap and the predict functions
@@ -65,7 +73,7 @@ compositions_umap <- umap(umap.data, n_neighbors = 10, min_dist = 0.4)
 
 ## Projecting test library
 
-predict(compositions_umap, umap.test) -> test_umap
+predict(compositions_umap, test.data) -> test_umap
 
 
 ## Preparing for plotting
@@ -110,26 +118,14 @@ compositions_umap_results_plot
 #ggplotly(compositions_umap_results_plot, tooltip = "SRR")
 
 
+## Coordinates and metadata
 
+compositions_umap_results %>%
+  filter(UMAP1 > -12 & UMAP1 < 25) %>% 
+  filter(UMAP2 > -12 & UMAP2 < 12) %>%           #filtering to the displayed area
+  mutate(lib_type = if_else(lib_type == "miRNA-Seq", "small_RNA-Seq", lib_type)) %>% 
+  mutate(lib_type = if_else(lib_type == "ncRNA-Seq", "small_RNA-Seq", lib_type)) %>% 
+  mutate(lib_type = if_else(lib_type == "ATAC-seq", "ATAC-Seq", lib_type)) %>% 
+  write_csv(nullfile())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ggsave(filename=args[6], path="/tmp", device="png", width = 6, height = 4, units = "in")
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,46 +1,91 @@
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
-
+use actix_web::{App, HttpResponse, HttpServer, Responder, middleware, web};
+use actix_cors;
 use fastq2comp::BaseComp;
-use std::{fs::canonicalize, process::{Command, Stdio}, io::Write};
-
+use std::{env, fs::{File, canonicalize}, io::{Read, Write}, process::{Command, Stdio}};
+use simple_logger::SimpleLogger;
+use log;
 
 async fn plot_comp(comp: web::Json<BaseComp>) -> impl Responder {
     let mut input = comp.into_inner().lib.into_iter().flat_map(|b| b.bases.iter()).
         fold(String::new(), |acc, curr| acc + &curr.to_string() + "\t");
     input.pop(); // remove trailing ',' to make it valid tsv
 
-    let mut child = Command::new("sh")
-        .current_dir(canonicalize("..").unwrap())
-        .arg("Rscript")
-        .arg("scripts/placeholder_code_for_graph_210726.R")
-        .stdin(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn child process");
-        
-    let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    std::thread::spawn(move || {
-        stdin.write_all(input.as_bytes()).expect("Failed to write to stdin");
-    });
-    
-    let output = child.wait_with_output().expect("Failed to read stdout");
-    let output = String::from_utf8_lossy(&output.stdout);
+    let output = web::block(move || {
+        let tmpfile = dbg!(
+            String::from_utf8_lossy(
+                &Command::new("mktemp")
+                    .output()
+                    .expect("Temporary file creation failed.")
+                .stdout
+                // removes the \n which mktemp appends
+            ).to_string().split_ascii_whitespace().next().unwrap().to_owned()
+        );
 
-    HttpResponse::Ok()
-            .content_type("text/csv")
-            .body(output.to_string())
+        let mut child = Command::new("Rscript")
+            .current_dir(dbg!(canonicalize("../").unwrap()))
+            .stdin(Stdio::piped())
+            .stdout(Stdio::inherit())
+            .arg("scripts/placeholder_code_for_graph_210726.R")
+            .arg(&dbg!(std::path::PathBuf::from(tmpfile.clone()).file_name().unwrap()))
+            .spawn()
+            .expect("Failed to spawn child process");
+
+        eprintln!("Writing to Rscript stdin: {:?}", input);
+
+        let mut stdin = child.stdin.take().expect("Failed to open stdin");
+
+        std::thread::spawn (move ||
+            stdin.write_all(input.as_bytes()).expect("Failed to write to stdin")
+        );
+
+        match child.wait() {
+            Ok(e) => {
+                if !e.success() {return Err(format!("{:?}", e))};
+                Ok({
+                    let mut f = File::open(tmpfile).expect("Unable to open tempfile");
+                    let mut o = Vec::new();
+                    if f.read_to_end(&mut o).expect("Unable to read tempfile") == 0 {
+                        panic!("Image file is empty.")
+                    }
+                    o
+                })
+            },
+            Err(e) => Err(format!("{:?}", e))
+        }
+    }).await;
+
+    match output {
+        Ok(o) => HttpResponse::Ok()
+            .content_type("image/png")
+            .body(o),
+        Err(_e) => HttpResponse::InternalServerError().finish()
+    }
+
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    eprintln!("Starting application");
+
     HttpServer::new(|| {
+        let cors = actix_cors::Cors::permissive();
+        SimpleLogger::new().with_level(log::LevelFilter::Trace).init().unwrap();
+
         App::new()
+            .wrap(cors)
+            .wrap(middleware::Logger::default())
             .service(
                 web::scope("/api")
                     .route("/plot_comp", web::post().to(plot_comp))
-            )            
-            .route("/{name}", web::get().to(plot_comp))
+            )
     })
-    .bind(("127.0.0.1", 80))?
+    .bind(("127.0.0.1", {
+        env::var("PORT")
+            .ok()
+            .and_then(|port| port.parse().ok())
+            .unwrap_or_else(|| 8186)
+        }
+    ))?
     .run()
     .await
 }
