0e52a12543e5a017a8b414c1bf459d0ddd9b0797
Kartavya Vashishtha <sendtokartavya@gmail.com>
2022-11-24 23:15:47 +0530

    feat: add target_reads arg in fastq2comp example

--- fastq2comp/README.md
+++ fastq2comp/README.md
@@ -2,6 +2,6 @@
 
 Library to read base compositions from FASTQ files. Given a valid file, produces a the percentages of each base per position.
 
-For an overview of usage, check out the [example program](examples/extract-comp/main.rs). You can also test the library through the example by replacing `in.fastq` with your file and looking at `out.json` (run ``cargo run --example extract-comp``)
+For an overview of usage, check out the [example program](examples/extract-comp/main.rs). You can also test the library through the example by replacing `in.fastq` with your file and looking at `out.json` (run ``cargo run --example extract-comp -- 100 "examples/extract-comp/in.fastq"``)
 
 You can view the crate documentation via `cargo doc --open`.
--- fastq2comp/examples/extract-comp/main.rs
+++ fastq2comp/examples/extract-comp/main.rs
@@ -1,19 +1,38 @@
+/**
+ * Run with the command:
+ * cargo run --example extract-comp -- 100 "examples/extract-comp/in.fastq"
+ */
+
 use fastq2comp::extract_comp::FASTQReader;
 use fastq2comp::extract_comp::{run_json, SampleArgs};
 use fastq2comp::io_utils;
 
+use clap::Parser;
+
 use std::fs::File;
 use std::{io::Write, path::PathBuf};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Target read count
+    target_read_count: u64,
+
+    /// File to extract reads from.
+    file: String // this should technically be a PathBuf... but non-UTF paths
+}
 
 /// This example extracts the base composition of a file
 /// and prints it JSON format.
 
 fn main() {
-    let path = "examples/extract-comp/in.fastq";
+    let cli = Cli::parse();
+
+    let path = cli.file;
     let f = File::open(path).unwrap();
     let mut reader = io_utils::compressed_reader(f, false);
 
-    let result = run_json(FASTQReader::new(SampleArgs::default(), &mut reader));
+    let result = run_json(FASTQReader::new(SampleArgs {target_read_count: cli.target_read_count, ..SampleArgs::default()}, &mut reader));
 
     let mut file = match File::create(&PathBuf::from("examples/extract-comp/out.json")) {
         Err(why) => panic!("Couldn't open output JSON file: {}", why),
